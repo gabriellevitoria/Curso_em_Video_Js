41fcc70c3be61bf5feb414a5da25dec5f5a4ef1e
Gabrielle Vitória <102485445+gabriellevitoria@users.noreply.github.com>
2023-03-05 18:42:52 -0300

    Add files via upload

---
+++ ambiente.js
@@ -0,0 +1,26 @@
+//E.R -> Teste no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}  while (c <= 6) 
+
+
+// E.R -> Teste no início
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} 
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
